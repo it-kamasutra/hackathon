3fb3b85d856410cf5242e48bcf518456944c4f75
kuzyuberdin <kuzyuberdin@gmail.com>
2019-09-08 16:50:31 +0300

    fixed reducer

--- front/src/domain/reducer.js
+++ front/src/domain/reducer.js
@@ -1,18 +1,12 @@
-
+const ADD_PLAYER = "ADD_PLAYER";
+const ADD_GAME = "ADD_GAME";
+const CHANGE_PLAYER = "CHANGE_PLAYER";
 
 const initialState = {
     players: [
         {
             _id: "1",
             fullName: "Seryoja Borovik"
-        },
-        {
-            _id: "2",
-            fullName: "Lesha"
-        },
-        {
-            _id: 3,
-            fullName: "Dimych"
         }
     ],
     games: [
@@ -34,10 +28,50 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_PLAYER: {
+            return {
+                ...state,
+                players: [...state.players, {_id: action._id, fullName: action.fullName}]
+            }
+        }
+        case ADD_GAME: {
+            return {
+                ...state,
+                games: [...state.games, {
+                    startDate: action.startDate,
+                    endDate: action.endDate,
+                    _id: action._id,
+                    player1: {
+                        _id: action._id1,
+                        winCount: action.winCount1
+                    },
+                    player2: {
+                        _id: action._id2,
+                        winCount: action.winCount2
+                    }
+                }]
+            }
+        }
+        case CHANGE_PLAYER: {
+            return {
+                ...state,
 
+            }
+        }
         default:
             return state
     }
+}
+
+const addPlayer = (_id, fullName) => {
+    return { type: ADD_PLAYER, _id, fullName };
+}
+const addGame = (startDate, endDate, _id, _id1, _id2, winCount1, winCount2) => {
+    return { type: ADD_GAME, startDate, endDate, _id, _id1, _id2, winCount1, winCount2 };
+}
+
+const changePlayer = (newPlayerName) => {
+    return {type: CHANGE_PLAYER, newPlayerName};
 }
 
 export default reducer;
