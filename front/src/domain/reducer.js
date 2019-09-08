b09a85b4c3aaadca749a52352700c834bb64a56c
Dimych <kuzyuberdin@gmail.com>
2019-09-08 13:35:37 +0300

    init commit

---
+++ front/src/domain/reducer.js
@@ -0,0 +1,30 @@
+const initialState = {
+    players: [
+        {
+            id: 1,
+            fullName: "Seryoja Borovik"
+        },
+        {
+            id: 2,
+            fullName: "Lesha"
+        },
+        {
+            id: 3,
+            fullName: "Dimych"
+        }
+    ],
+    games: [
+        {
+            player1: {
+                id: 1,
+                winCount: 1
+            },
+            player2: {
+                id: 1,
+                winCount: 1
+            }
+        }
+    ]
+
+
+}
