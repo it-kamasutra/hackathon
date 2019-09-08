8661b101457ce62a09697320e11b783c75029745
Dimych <kuzyuberdin@gmail.com>
2019-09-08 16:17:03 +0300

    rename UIComponents to ui

--- front/src/index.js
+++ front/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import App from "./UIComponents/App";
+import App from "./ui/App";
 import {Provider} from "react-redux";
 import store from "./domain/store";
 
